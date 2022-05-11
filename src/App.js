32c8b311b4c83e55fa598c6a31dd6cbdede0f30f
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-11 10:57:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react"
 import './App.css';
 
 function App() {
+  
+  const [number, setNumber] = useState(1)
+  const [fizzbuzz, setFizzbuzz] = useState(1)
+
+  useEffect(()=>{
+    if (number % 3 === 0 && number % 5 === 0) {
+      setFizzbuzz("fizzbuzz")
+    } else if (number % 3 === 0) {
+      setFizzbuzz("fizz")
+    } else if (number % 5 === 0) {
+      setFizzbuzz("buzz")
+    } else {
+      setFizzbuzz(number);
+    }
+
+  })
+  
+  const handleInc = () => {
+    setNumber(number+1)
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <button value={number} onClick={handleInc}>+ add</button>
+
+    <h2>Number is {number}</h2>
+    <h2>{fizzbuzz}</h2>
+
+    </>
   );
 }
 
