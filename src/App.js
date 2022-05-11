fa32586da5de6f0244417a9b88ffc910635882a9
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-11 13:13:43 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ function App() {
   const [number, setNumber] = useState(1)
   const [fizzbuzz, setFizzbuzz] = useState(1)
   const [name, setName] = useState("")
+  const [userInput, setUserInput] = useState("")
+  const [pointlessState, setpointlessState] = useState(0)
+
+
+  
 
   useEffect(()=>{
-    if (number % 3 === 0 && number % 5 === 0) {
-      setFizzbuzz("fizzbuzz")
-    } else if (number % 3 === 0) {
-      setFizzbuzz("fizz")
-    } else if (number % 5 === 0) {
-      setFizzbuzz("buzz")
-    } else {
-      setFizzbuzz(number);
-    }
+    
+      if (number % 3 === 0 && number % 5 === 0) {
+        setFizzbuzz("fizzbuzz")
+      } else if (number % 3 === 0) {
+        setFizzbuzz("fizz")
+      } else if (number % 5 === 0) {
+        setFizzbuzz("buzz")
+      } else {
+        setFizzbuzz(number);
+      } 
 
   }, [number])
 
@@ -37,13 +43,31 @@ function App() {
     setNumber(number+1)
   }
 
+  const handleuserInputChange = (event) => {
+    setUserInput(event.target.value)
+  }
+
+  const handlePointlessButton = () => {
+    setpointlessState(pointlessState + 1 )
+  } 
+
   
   return (
     <>
+    <button onClick={handlePointlessButton} > hello I do nothing</button>
+    <p><b> the poitnless state number is{pointlessState}</b></p>
+    
 
-    <button value={number} onClick={handleInc}>+ add</button>
+    <h2>Number is {number}</h2>
+    <button value={number} onClick={handleInc}>+ add to number</button>
+    <h3>the correct answer is: {fizzbuzz}</h3>
+   {userInput == fizzbuzz ? <p>Correct!</p> : <p>keep guessing!</p>}
+    
+    {/* <form onSubmit={handleUserInput}> */}
+    <form>
+      <input type="text" value={userInput} placeholder="make your guess" onChange={handleuserInputChange} />
+    </form>
 
-    <h2>Number is {number}: {fizzbuzz}</h2>
     <h3>{name}</h3>
 
     </>
